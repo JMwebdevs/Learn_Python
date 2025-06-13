4e7b54ba53f62441fb9e27f5bb8568eca714deac
JMwebdevs <lapenidjohnmark@gmail.com>
2025-06-13 13:49:20 +0800

    Day - 12 | String Indexing

---
+++ Day 12/index.py
@@ -0,0 +1,9 @@
+
+cpNumber = "0970-113-3410"
+# result= cpNumber[1]
+# result= cpNumber[0:]
+# result= cpNumber[:13]
+# result= cpNumber[0:4]
+# result= cpNumber[-1]
+result= cpNumber[::3]
+print(result)
