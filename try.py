910777bdedf21bdea80b7d2fee4e71f207b646a3
JMwebdevs <lapenidjohnmark@gmail.com>
2025-07-17 23:45:05 +0800

    Day - 16 | Python Compound Interest Calculator

---
+++ try.py
@@ -0,0 +1,12 @@
+# arr = [2, 3, 3, 1, 6, 2]
+# target = 6
+
+# for elem in arr:
+#   if elem == target:
+#     print(True)
+
+arr = [2, 3, 3, 1, 6,] 
+target = 5
+for elem in arr:
+  if elem == target:
+    print(False) 
